a599f9c18499e99c8ff02ec003f449aa13056841
Graham <u8360182@gmail.com>
2025-05-07 21:02:26 +0800

    style(login): update login page styles
    Refined SCSS structure to enhance the layout and design of the login page, including background image adjustments, container dimensions, and title styling.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,34 @@
+import logo from '@/assets/logo.png'
+import backgroundPicture from '@/assets/bg.jpg'
+import loginBackground from '@/assets/login-background.png'
+import './index.scss'
+
 function Login() {
+
+  const backgroundStyle: React.CSSProperties = {
+    backgroundImage: `url(${backgroundPicture})`,
+  };
+
+
+  const loginStyle: React.CSSProperties = {
+    backgroundImage: `url(${loginBackground})`,
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
+    <div className='login' style={backgroundStyle}>
+      <div className='container'>
+        <div className='login-background' style={loginStyle}>
+        </div>
+        <div className='part'>
+          <div className="title">
+            <div className="logo">
+              <img src={logo} alt="Logo" width={100} />
+            </div>
+            <h1>管理平台</h1>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
