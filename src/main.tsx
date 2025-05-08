ead24edb21d5f3666af82451a5ac89412d107899
Graham <u8360182@gmail.com>
2025-05-08 16:19:48 +0800

    feat(login): 新增登入頁面功能與樣式
    新增使用者名稱與密碼的輸入欄位，並整合 Ant Design 的表單元件以進行驗證。

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root') as HTMLElement)
   .render(
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,9 @@ import logo from '@/assets/logo.png'
 import backgroundPicture from '@/assets/bg.jpg'
 import loginBackground from '@/assets/login-background.png'
 import './index.scss'
+import { Button, Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
 
 function Login() {
 
@@ -13,6 +16,19 @@ function Login() {
   const loginStyle: React.CSSProperties = {
     backgroundImage: `url(${loginBackground})`,
   };
+
+  const [form] = Form.useForm();
+
+  function handleSubmit() {
+    form.validateFields()
+      .then(values => {
+        console.log('Received values:', values);
+        // Handle login logic here
+      })
+      .catch(errorInfo => {
+        console.error('Validation failed:', errorInfo);
+      });
+  }
 
   return (
     <div className='login' style={backgroundStyle}>
@@ -26,6 +42,31 @@ function Login() {
             </div>
             <h1>管理平台</h1>
           </div>
+          <Form
+            form={form}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input placeholder='輸入帳號' prefix={<UserOutlined />} />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password placeholder='輸入密碼' prefix={<LockOutlined />} />
+            </Form.Item>
+
+            <Form.Item label={null}>
+              <Button type="primary" style={{ width: '100%' }} onClick={handleSubmit} >
+                登入
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
 
