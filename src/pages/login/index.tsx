2246b13fd5c94196baaca86d81e64eec3c6efc57
Graham <u8360182@gmail.com>
2025-05-13 21:37:20 +0800

    feat(login): 新增登入頁面加載狀態管理
    在登入過程中增加加載狀態。

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,12 +7,16 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { login } from '@/api/users';
 import { useDispatch } from 'react-redux';
 import { setToken } from '@/store/login/authSlice';
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
 
 
 function Login() {
   // hook
   const [form] = Form.useForm();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const backgroundStyle: React.CSSProperties = {
     backgroundImage: `url(${backgroundPicture})`,
@@ -27,15 +31,19 @@ function Login() {
   const handleSubmit = async () => {
     try {
       const values = await form.validateFields();
-
-
+      setLoading(true);
       const { token } = await login(values);
+      setLoading(false);
+
       // 將 token 存入 Redux store
       dispatch(setToken(token));
+      // 導向首頁
+      navigate('/');
       // 處理登入成功
       console.log('Login successful:', token);
 
     } catch (error) {
+      setLoading(false);
       console.error('Login failed:', error);
       // 處理登入失敗
     }
@@ -74,7 +82,7 @@ function Login() {
             </Form.Item>
 
             <Form.Item label={null}>
-              <Button type="primary" style={{ width: '100%' }} htmlType="submit" >
+              <Button type="primary" style={{ width: '100%' }} htmlType="submit" loading={loading}>
                 登入
               </Button>
             </Form.Item>
