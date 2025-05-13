b5d01d1a121ab12085ffc19ea91ce8b016818f0c
Graham <u8360182@gmail.com>
2025-05-13 16:39:38 +0800

    feat(mock): 更新登入模擬 API
    新增對登入請求的處理邏輯，並回傳相應的登入成功或失敗訊息。

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -6,15 +6,49 @@ const BASE_URL: string = import.meta.env.VITE_API_URL;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 Mock.mock(`${BASE_URL}/login`, "post", (options: any): any => {
 
-  if (options.body == null) {
-    return {};
+  const { account, password } = JSON.parse(options.body);
+
+  let response;
+
+  switch (`${account}:${password}`) {
+    case 'admin:123456':
+      response = {
+        code: 200,
+        message: '登入成功',
+        data: {
+          account: 'admin',
+          token: 'mocktoken123456admin',
+        }
+      };
+      break;
+    case 'user:123456':
+      response = {
+        code: 200,
+        message: '登入成功',
+        data: {
+          account: 'user',
+          token: 'mocktoken123456user'
+        }
+      };
+      break;
+    case 'manager:123456':
+      response = {
+        code: 200,
+        message: '登入成功',
+        data: {
+          account: 'manager',
+          token: 'mocktoken123456manager'
+        }
+      };
+      break;
+    default:
+      response = {
+        code: 401,
+        message: '帳號或密碼錯誤',
+        data: null
+      };
   }
-  return {
-    code: 200,
-    message: "success",
-    data: {
-      username: "admin",
-      token: "mocktoken123456"
-    }
-  };
+
+  return response;
+
 });
