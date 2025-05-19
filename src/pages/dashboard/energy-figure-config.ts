c286db29a99e2d376b0780cf516341878363f691
Graham <u8360182@gmail.com>
2025-05-19 19:18:55 +0800

    feat(dashboard): 新增能源消耗圖表配置

---
+++ src/pages/dashboard/energy-figure-config.ts
@@ -0,0 +1,33 @@
+const energyFigureConfig = {
+  title: {
+    text: 'Daily Energy Consumption',
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: []
+  },
+  grid: {
+    left: '%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['00:00', '04:00', '08:00', '12:00', '16:00', '20:00', '24:00']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: []
+};
+
+export default energyFigureConfig;
