9634a090614e490b80c8dcbab8b8986c42fb3a2a
Graham <u8360182@gmail.com>
2025-05-20 20:58:27 +0800

    feat(users): 更新客戶企業列表頁面，新增搜尋欄位以便過濾客戶資料

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,56 +1,130 @@
-import { Button, Card, Col, Input, Row, Table } from 'antd';
+import { Button, Card, Col, Input, Row, Table, type TableProps } from 'antd';
+import type { CompanyDataType } from './interface';
+import { useCallback, useEffect, useState } from 'react';
+import { getClientList } from '@/api/client-list';
 
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
 
-const columns = [
+const columns: TableProps<CompanyDataType>['columns'] = [
   {
-    title: 'Name',
+    title: 'Enterprise',
     dataIndex: 'name',
     key: 'name',
+    align: 'center',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    align: 'center',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Contact',
+    dataIndex: 'phoneNumber',
+    key: 'phoneNumber',
+    align: 'center',
   },
+  {
+    title: 'Industry Category',
+    dataIndex: 'industryCategory',
+    key: 'industryCategory',
+    align: 'center',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    align: 'center',
+  },
+  {
+    title: 'Unified Business Number',
+    dataIndex: 'unifiedBusinessNumber',
+    key: 'unifiedBusinessNumber',
+    align: 'center',
+  },
+  {
+    title: 'Industry Code',
+    dataIndex: 'industryCode',
+    key: 'industryCode',
+    align: 'center',
+  },
+  {
+    title: 'Responsible Person',
+    dataIndex: 'responsiblePerson',
+    key: 'responsiblePerson',
+    align: 'center',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    dataIndex: 'action',
+    align: 'center',
+  }
 ];
 
-<Table dataSource={dataSource} columns={columns} />;
+interface searchType {
+  name: string;
+  phone: string;
+  contact: string;
+}
+
 
 const Users: React.FC = () => {
+  const [dataList, setDataList] = useState<CompanyDataType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [searchOpt, setSearchOpt] = useState<searchType>({
+    name: '',
+    phone: '',
+    contact: ''
+  });
+  const [total, setTotal] = useState<number>(0);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSearchOpt((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  const loadData = useCallback(async () => {
+    // 模擬 API 請求
+    try {
+      setLoading(true);
+      const { data: { list, total } } = await getClientList({
+        ...searchOpt,
+        page,
+        pageSize
+      });
+      setLoading(false);
+
+      setTotal(total);
+
+      setDataList(list);
+    }
+    catch (error) {
+      setLoading(false);
+      console.error('Error fetching data:', error);
+    }
+  }, [page, pageSize, searchOpt]);
+  useEffect(() => { loadData(); }, [loadData]);
+
   return (
     <div className='users'>
       <Card className='search'>
         <Row gutter={16}>
           <Col span={7}>
             <span>Enterprise</span>
-            <Input></Input>
+            <Input name='name' value={searchOpt.name} onChange={handleSearchChange} />
           </Col>
           <Col span={7}>
             <span>Contact</span>
-            <Input></Input>
+            <Input name='contact' value={searchOpt.contact} onChange={handleSearchChange} />
           </Col>
           <Col span={7}>
             <span>Phone</span>
-            <Input></Input>
+            <Input name='phone' value={searchOpt.phone} onChange={handleSearchChange} />
           </Col>
           <Col span={3} style={{ paddingLeft: '30px' }}>
             <Button type='primary' style={{ marginRight: '8px' }}>Search</Button>
@@ -62,8 +136,13 @@ const Users: React.FC = () => {
         <Button type='primary' >New Enterprise</Button>
         <Button danger type='primary' className='ml'>Batch Delete</Button>
       </Card>
-      <Card>
-        <Table columns={columns} dataSource={dataSource}></Table>
+      <Card className='mt'>
+        <Table
+          columns={columns}
+          dataSource={dataList}
+          loading={loading}
+          rowKey={(record) => record.id}
+        />
       </Card>
     </div>
   );
