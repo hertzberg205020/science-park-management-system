407ab53b5d9329e14a9c3867651d15ca90dc4dfc
Graham <u8360182@gmail.com>
2025-05-20 11:29:15 +0800

    feat(dashboard): 新增出租狀態圓餅圖、充電樁可用性統計及即時車輛資訊

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,9 @@
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Progress, Row, Statistic, Tag, Timeline } from 'antd';
 import { icons } from './icons';
 import './index.scss'
 import EnergyFigure from './EnergyFigure';
 import BarFigure from './BarFigure';
+import PieFigure from './PieFigure';
 
 interface CardInfo {
   title: string;
@@ -30,6 +31,29 @@ const cardData: CardInfo[] = [
     title: '2874',
     description: '入駐企業數量(家)',
     icon: icons.LaptopOutlined,
+  },
+];
+
+const items: { children: React.ReactNode; color?: string }[] = [
+  {
+    children: <><Tag color="green">Entry</Tag>08:24 Vehicle A66666</>
+  },
+  {
+    children: <><Tag color="red">Exit</Tag>09:15 Vehicle A66666</>,
+    color: 'red',
+  },
+  {
+    children: <><Tag color="green">Entry</Tag>09:22 Vehicle A23456</>,
+  },
+  {
+    children: <><Tag color="red">Exit</Tag>10:43 Vehicle A18763</>,
+    color: 'red',
+  },
+  {
+    children: <><Tag color="green">Entry</Tag>13:38 Vehicle A88888</>,
+  },
+  {
+    children: <><Tag color="green">Entry</Tag>14:46 Vehicle A23456</>,
   },
 ];
 
@@ -66,6 +90,29 @@ const Dashboard: React.FC = () => {
         <Col span={12}>
           <Card title="園區產值概況" className='card'>
             <BarFigure />
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={16} className='mt'>
+        <Col span={12}>
+          <Card title="Rental Status">
+            {/* PieFigure */}
+            <PieFigure />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="Charging Pile Availability Statistics">
+            <div className="wrap" style={{ paddingTop: '60px' }}>
+              <Progress type="circle" percent={75} />
+              <Statistic title="Total Charging Piles" value={75} suffix="/ 100" className="mt" />
+            </div>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="Real-time Vehicle Information" style={{ height: "406px" }}>
+            <div className="wrap" style={{ paddingTop: '45px' }}>
+              <Timeline items={items} />
+            </div>
           </Card>
         </Col>
       </Row>
